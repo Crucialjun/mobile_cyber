6dc163ef397d54d3a5cf98d3eb84ef025a2e9cb1
Crucialjun <nich.otieno@gmail.com>
2023-06-17 09:24:05 +0300

    add screenutil

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:mobile_cyber/features/splash/screens/splash_screen.dart';
 import 'package:mobile_cyber/router.dart';
-
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -16,15 +15,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mobile Cyber',
-      theme: ThemeData(
-        fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
-      onGenerateRoute: (settings) => generateRoute(settings),
+    return ScreenUtilInit(
+      designSize: const Size(414, 736),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Mobile Cyber',
+          theme: ThemeData(
+            fontFamily: GoogleFonts.spaceGrotesk().fontFamily,
+            primarySwatch: Colors.blue,
+          ),
+          home: const SplashScreen(),
+          onGenerateRoute: (settings) => generateRoute(settings),
+        );
+      },
     );
   }
 }
